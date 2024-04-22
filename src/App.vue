9a80ea2fbe9c6e48bd151854fbffa0bb01f6e178
Alison Moura <alison.oghino@gmail.com>
2024-04-22 12:21:31 -0400

    Inciando projeto web Vuejs do Betnget

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AppNavBar from './components/AppNavBar.vue'
+</script>
+
+<template>
+  <AppNavBar></AppNavBar>
+  <main>
+  </main>
+</template>
---
+++ src/components/AppNavBar.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+<template>
+  <nav class="nav-bar">
+    <h1>Betnget</h1>
+    <ul>
+      <li>USUARIO</li>
+      <li>PROMOÇÕES</li>
+      <li>CUPONS</li>
+      <li>LOGIN</li>
+    </ul>
+  </nav>
+</template>
