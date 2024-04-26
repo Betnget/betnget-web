86426db729d92d5421aa8e654d86138e40a2ae37
Jose Weiler <jgcarmoweiler@gmail.com>
2024-04-26 12:24:45 -0400

    Criando navegacao pagina Usuarios

--- src/components/AppNavBar.vue
+++ src/components/AppNavBar.vue
@@ -4,7 +4,7 @@
     <h1>Betnget</h1>
     <ul>
       <RouterLink to="/">INÍCIO</RouterLink>
-      <li>USUARIO</li>
+      <RouterLink to="/usuarios">USUÁRIOS</RouterLink>
       <RouterLink to="/promocoes">PROMOÇÕES</RouterLink>
       <li>CUPONS</li>
       <li>LOGIN</li>
--- src/plugins/vue-router.js
+++ src/plugins/vue-router.js
@@ -2,13 +2,15 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import Home from "@/pages/Home.vue";
 import Promocoes from "@/pages/Promocoes.vue";
+import Usuarios from "@/pages/Usuarios.vue";
 
 const routes = [
-    { path: "/", component: Home },
-    { path: "/promocoes", component: Promocoes },
-]
+  { path: "/", component: Home },
+  { path: "/promocoes", component: Promocoes },
+  { path: "/usuarios", component: Usuarios },
+];
 
 export const router = createRouter({
-    history: createWebHistory(),
-    routes: routes,
+  history: createWebHistory(),
+  routes: routes,
 });
